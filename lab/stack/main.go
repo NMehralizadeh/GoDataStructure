2ca11189143249b7fbc5ce3f511c02c426635e11
Navid Mehralizadeh <cemorgh@gmail.com>
2019-02-12 00:58:01 +0330

    main test

--- lab/stack/main.go
+++ lab/stack/main.go
@@ -4,14 +4,43 @@ import (
 	"fmt"
 )
 
-func main() {
-	t := stack{
-		value: "navid",
-	}
+type Stack struct {
+	Value interface{}
+	Next  *Stack
+}
 
-	t.Push("David")
-	t.Push("Hassan")
-	fmt.Println(t.Pull())
-	fmt.Println(t.Pull())
-	fmt.Println(t.Pull())
+func (s *Stack) New(value interface{}) {
+	s = &Stack{
+		Value: value,
+	}
+}
+
+func (s *Stack) Pull() interface{} {
+	value := s.Value
+	s = s.Next
+	return value
+}
+
+func (s *Stack) Push(stackValue interface{}) {
+	s = &Stack{Value: stackValue, Next: s}
+	fmt.Println(s)
+}
+
+func main() {
+	stack := Stack{}
+
+	stack.New("Navid")
+
+	fmt.Println("PPPPP")
+	p := &stack
+	fmt.Println(*p)
+	fmt.Println(stack)
+
+	stack.Push("David")
+	stack.Push("Hassan")
+
+	fmt.Println(stack)
+	fmt.Println(stack.Pull())
+	fmt.Println(stack.Pull())
+	fmt.Println(stack.Pull())
 }
